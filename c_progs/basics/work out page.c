0e708e361f321e05e73fb240a02e29aabf7824d1
Salha Nasreen M <88109792+SNasreen@users.noreply.github.com>
2023-07-22 15:59:40 +0530

    Delete c_progs directory

--- c_progs/basics/work out page.c
+++
@@ -1,9 +0,0 @@
-int main()
-{
-    int b=25;
-    //b memory location=1234;
-    int *p = &b;
-    printf("%d %d", b, *p);
-
-    return 0;
-}
--- c_progs/basics/working.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    char a[1000];
-    gets(a);
-    int up=0;
-}
